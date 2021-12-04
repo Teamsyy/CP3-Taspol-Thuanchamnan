9bfbccebee7e9bd0693f658e36a84663f4a2f67f
Teamsyy <meowmeow2346@gmail.com>
2021-12-04 16:01:02 +0700

    Sent

---
+++ Exercise5_1_Taspol.py
@@ -0,0 +1,8 @@
+a =int(input("Enter Your Number : "))
+b = int(input("Enter Your Number : "))
+print("Your Number is",a,"and",b)
+
+print( a ,"+",b,"=",a+b)
+print( a ,"-",b,"=",a-b)
+print( a ,"*",b,"=",a*b)
+print( a ,"/",b,"=",a/b)
