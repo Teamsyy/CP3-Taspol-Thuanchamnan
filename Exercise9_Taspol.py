28a6dc22f1c82c772c1ae5914b5a20093da00eba
Teamsyy <meowmeow2346@gmail.com>
2021-12-07 01:19:29 +0700

    sent

---
+++ Exercise9_Taspol.py
@@ -0,0 +1,8 @@
+userName = input("Username : ")
+passWord = input("Password : ")
+while userName !="team800" or passWord !="teamsy":
+    print("Fail Login")
+    userName = input("Username : ")
+    passWord = input("Password : ")
+
+print("Success Login")
